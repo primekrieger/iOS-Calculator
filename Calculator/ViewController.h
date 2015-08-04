efeff8744c0f6cb981395bb8a9e7e96d9135b17b
Diwakar Kamboj <diwakarkamboj@Diwakars-MacBook-Pro.local>
2015-08-04 16:52:56 +0530

    Basic

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -10,6 +10,23 @@
 
 @interface ViewController : UIViewController
 
+{
+    IBOutlet UILabel *inputLabel;
+    IBOutlet UILabel *resultLabel;
+    
+    NSMutableString *input;
+    float num1;
+    float num2;
+    float result;
+    float *currentNum;
+    unichar currentOperator;
+    int operatorAppendFlag;
+    
+}
+
+- (IBAction)digitTap:(UIButton *)sender;
+- (IBAction)operatorTap:(UIButton *)sender;
+- (float)calculate;
 
 @end
 
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -17,11 +17,88 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    currentNum = &num1;
+    //input = [[NSMutableString alloc] init];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)digitTap:(UIButton *)sender
+{
+    if (!input)    // Check whether the NSMutableString to be set as label text has been initialized or not
+    {
+        input = [[NSMutableString alloc] initWithString:[sender currentTitle]];    // Initialize the string with the tapped button text
+        inputLabel.textAlignment = NSTextAlignmentRight;    // Set text alignment to left
+    }
+    
+    else
+    {
+        [input appendString:[sender currentTitle]];    // Append the tapped button text to current string
+    }
+    
+    inputLabel.text = input;
+    
+    *currentNum = (*currentNum * 10) + [[sender currentTitle] intValue];
+    
+    if (currentNum == &num2)
+    {
+        result = [self calculate];
+        resultLabel.text = [NSString stringWithFormat:@"%g", result];
+    }
+    
+    operatorAppendFlag = 1;
+}
+
+- (IBAction)operatorTap:(UIButton *)sender
+{
+    currentOperator = [[sender currentTitle] characterAtIndex:0];
+    
+    if (operatorAppendFlag) {
+        [input appendString:[sender currentTitle]];
+        inputLabel.text = input;
+        operatorAppendFlag = 0;
+    }
+    
+    if (currentNum == &num2)
+    {
+        num1 = result;
+        num2 = 0;
+        resultLabel.text = [NSString stringWithFormat:@"%g", num1];
+    }
+    
+    else
+    {
+        currentNum = &num2;
+    }
+}
+
+- (float)calculate
+{
+    switch (currentOperator) {
+        case '+':
+            return num1 + num2;
+            break;
+            
+        case '-':
+            return num1 - num2;
+            break;
+            
+        case '*':
+            return num1 * num2;
+            break;
+            
+        case '/':
+            return num1 / num2;
+            break;
+            
+        default:
+            return -1;
+    }
 }
 
 @end
